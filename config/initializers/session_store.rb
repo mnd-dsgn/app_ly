46b692ec8f1ddbbccecce00d758120277ceea9d3
“Matthew <matthew.hinea@gmail.com>
2016-08-29 08:41:01 -0700

    remove domain param from session_store config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_college_scorecard_session', domain: 'http://floating-retreat-44813.herokuapp.com'
+Rails.application.config.session_store :cookie_store, key: '_college_scorecard_session' #, domain: 'http://floating-retreat-44813.herokuapp.com'
